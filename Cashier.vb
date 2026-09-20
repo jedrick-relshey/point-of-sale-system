33c3cb9e5851accbb4c037b0c84a3edd97f59fb6
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-09-20 23:31:17 +0800

    added a reset buton colors

--- Cashier.vb
+++ Cashier.vb
@@ -1,11 +1,43 @@
 ﻿Public Class Cashier
-    Private Sub Guna2Button7_Click(sender As Object, e As EventArgs) Handles Guna2Button7.Click
+
+    Private Sub HideAllPanels()
+
+        pnltest.Visible = False
+        dashboardpanel.Visible = False
 
     End Sub
 
-    Private Sub BindingNavigatorPositionItem_Click(sender As Object, e As EventArgs)
+    Private Sub ResetButtonColors()
+        btn_DashBoard.BackColor = Color.Transparent
+        btn_Point_Of_Sale.BackColor = Color.Transparent
+        btn_prdt.BackColor = Color.Transparent
+        btn_invtry.BackColor = Color.Transparent
+        btn_hstry.BackColor = Color.Transparent
 
+        btn_DashBoard.ForeColor = Color.DarkGray
+    End Sub
 
+    Private Sub btn_DashBoard_Click(sender As Object, e As EventArgs) Handles btn_DashBoard.Click
+
+        HideAllPanels()
+        dashboardpanel.Visible = True
+
+        ResetButtonColors()
+
+        btn_DashBoard.BackColor = Color.MistyRose
+        btn_DashBoard.ForeColor = Color.Black
+
+    End Sub
+
+    Private Sub btn_Point_Of_Sale_Click(sender As Object, e As EventArgs) Handles btn_Point_Of_Sale.Click
+
+        HideAllPanels()
+        pnltest.Visible = True
+
+        ResetButtonColors()
+
+        btn_Point_Of_Sale.BackColor = Color.MistyRose
+        btn_Point_Of_Sale.ForeColor = Color.Black
 
     End Sub
 End Class
